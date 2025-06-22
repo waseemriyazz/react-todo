7e31702f09885ed64dda2f2314de8333e7ccc5d7
waseemriyazz <waseemriyaz717@gmail.com>
2025-06-22 21:43:32 +0530

    inline styling a component

--- Todo/src/App.jsx
+++ Todo/src/App.jsx
@@ -1,9 +1,10 @@
+import InlineComponent from "./components/InlineComponent";
 import Todo from "./components/Todo";
 
 function App() {
   return (
     <div className="App">
-      <Todo />
+      <InlineComponent />
     </div>
   );
 }
---
+++ Todo/src/components/InlineComponent.jsx
@@ -0,0 +1,8 @@
+const header = { color: "blue", fontSize: "120px" };
+export default function InlineComponent() {
+  return (
+    <div>
+      <h1 style={header}>This is a header</h1>
+    </div>
+  );
+}
