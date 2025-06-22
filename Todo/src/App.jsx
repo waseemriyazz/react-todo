e57dace1b3c724b10f77a323325dd9f650e412cc
waseemriyazz <waseemriyaz717@gmail.com>
2025-06-22 20:53:51 +0530

    submitting todos

--- Todo/src/App.jsx
+++ Todo/src/App.jsx
@@ -1,5 +1,11 @@
+import Todo from "./components/Todo";
+
 function App() {
-  return <div className="App">hi</div>;
+  return (
+    <div className="App">
+      <Todo />
+    </div>
+  );
 }
 
 export default App;
---
+++ Todo/src/components/Todo.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+export default function Todo() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  function handleSubmit(e) {
+    e.preventDefault();
+    setTodos([...todos, todo]);
+
+    setTodo("");
+  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={(e) => setTodo(e.target.value)}
+          type="text"
+          value={todo}
+        />
+        <button>Add</button>
+      </form>
+      {console.log(todos)}
+    </div>
+  );
+}
